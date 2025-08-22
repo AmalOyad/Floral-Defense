532b0320766318573cc076df20318db21300a1e5
Impasta2048 <139719703+Coder20488@users.noreply.github.com>
2025-08-21 19:58:20 -0700

    added bug code to butterflies and changed names

--- butterfly_spawner.gd
+++ butterfly_spawner.gd
@@ -1,18 +1,32 @@
 extends Node2D
 
-# List of butterfly scenes (PackedScene resources)
-@export var butterfly_scenes: Array[PackedScene]
+@export var butterfly_scene: PackedScene
+@export var flower: Node2D
+@export var spawn_delay_range := Vector2(1, 3)
+@export var spawn_area: Rect2 = Rect2(0,0,1920,50)
+
+var timer := 0.0
+var next_spawn := 0.0
 
 func _ready():
-	spawn_random_butterfly()
+	randomize()
+	set_next_spawn()
 
-func spawn_random_butterfly():
-	if butterfly_scenes.is_empty():
-		return
-	
-	var random_index = randi() % butterfly_scenes.size()
-	var butterfly_scene = butterfly_scenes[random_index]
+func _process(delta):
+	timer += delta
+	if timer >= next_spawn:
+		spawn_butterfly()
+		set_next_spawn()
+
+func set_next_spawn():
+	timer = 0
+	next_spawn = randf_range(spawn_delay_range.x, spawn_delay_range.y)
+
+func spawn_butterfly():
 	var butterfly = butterfly_scene.instantiate()
-	
+	butterfly.global_position = Vector2(
+		randf_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x), 
+		spawn_area.position.y
+	)
+	butterfly.target = flower
 	add_child(butterfly)
-	butterfly.position = Vector2(0, 0) # change spawn position if needed
